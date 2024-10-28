17e84aa3232c24e093eeed1b659cb96967d2791c
Rubaiyat Rahman <94232498+Rubaiyat90@users.noreply.github.com>
2024-10-28 20:28:40 +0600

    final_project

---
+++ pixel-positions/resources/views/components/job-cards.blade.php
@@ -0,0 +1,15 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Title</div>
+    <div class="py-8 font-bold">
+        <h3>Name</h3>
+        <p>Salary</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tags>Tag</x-tags>
+            <x-tags>Tag</x-tags>
+            <x-tags>Tag</x-tags>
+        </div>
+        <img src="http://placehold.it/42/42" alt="" class="rounded">
+    </div>
+</div>
--- pixel-positions/tailwind.config.js
+++ pixel-positions/tailwind.config.js
@@ -1,15 +1,17 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
-        "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors:{
+                "black": "#060606"
+            }
+        },
     },
     plugins: [],
 };
