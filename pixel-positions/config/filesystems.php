4ba3376ad713740708e4a3c316edc2c4b1cb3e19
Rubaiyat Rahman <94232498+Rubaiyat90@users.noreply.github.com>
2024-12-18 18:11:49 +0600

    feat: Database backup added for storing in googe drive

--- pixel-positions/config/filesystems.php
+++ pixel-positions/config/filesystems.php
@@ -32,7 +32,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/private'),
+            'root' => storage_path('app'),
             'serve' => true,
             'throw' => false,
         ],
@@ -55,6 +55,14 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
+        ],
+
+        'google' => [
+            'driver' => 'google',
+            'clientId' => env('GOOGLE_DRIVE_CLIENT_ID'),
+            'clientSecret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
+            'refreshToken' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
+            'folder' => env('GOOGLE_DRIVE_FOLDER'),
         ],
 
     ],
